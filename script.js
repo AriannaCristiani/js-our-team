6a836425a98d0ab49dcc4bf408700afaf90b9901
AriannaCrisriani <ariannacristiani@yahoo.it>
2024-10-28 15:41:33 +0100

    creazione prima card: marco

---
+++ script.js
@@ -0,0 +1,54 @@
+
+
+
+const teamMembers = [
+  {
+    name: "Marco Bianchi",
+    role: "Designer",
+    email: "marcobianchi@team.com",
+    img: "img/male1.png"
+  },
+  {
+    name: "Laura Rossi",
+    role: "Front-end Developer",
+    email: "laurarossi@team.com",
+    img: "img/female1.png"
+  },
+  {
+    name: "Giorgio Verdi",
+    role: "Back-end Developer",
+    email: "giorgioverdi@team.com",
+    img: "img/male2.png"
+  },
+  {
+    name: "Marta Ipsum",
+    role: "SEO Specialist",
+    email: "martarossi@team.com",
+    img: "img/female2.png"
+  },
+  {
+    name: "Roberto Lorem",
+    role: "SEO Specialist",
+    email: "robertolorem@team.com",
+    img: "img/male3.png"
+  },
+  {
+    name: "Daniela Amet",
+    role: "Analyst",
+    email: "danielaamet@team.com",
+    img: "img/female3.png"
+  }
+];
+
+
+const marcosImgProfile = document.getElementById('marco-picture')
+const marcosName = document.getElementById('marco-name')
+const marcosRole = document.getElementById('marco-role')
+const marcosEmail = document.getElementById('marco-email')
+
+marcosImgProfile.src =  teamMembers[0].img
+marcosName.innerText = teamMembers[0].name
+marcosRole.innerText = teamMembers[0].role
+marcosEmail.innerHTML = teamMembers[0].email
+
+
